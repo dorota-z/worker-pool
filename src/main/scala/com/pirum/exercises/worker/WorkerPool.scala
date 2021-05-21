708f6ed6bdac57ef96bd6adead30df2837b3dc34
Regis Kuckaertz <629976+regiskuckaertz@users.noreply.github.com>
2021-05-21 18:29:27 +0100

    A good starting point

--- src/main/scala/com/pirum/exercises/worker/WorkerPool.scala
+++ src/main/scala/com/pirum/exercises/worker/WorkerPool.scala
@@ -3,24 +3,35 @@ package com.pirum.exercises.worker
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
+import scala.concurrent.Promise
+import java.util.concurrent.Semaphore
+import java.util.concurrent.atomic.AtomicInteger
 
-class WorkerPool(workers: Int) {
-  private val execService = Executors.newFixedThreadPool(workers)
-  private val execContext = ExecutionContext.fromExecutorService(execService)
+class WorkerPool(workers: Int)(implicit ec: ExecutionContext) {
+  private val execService = Executors.newScheduledThreadPool(1)
+  private val queue = collection.mutable.Queue.empty[Promise[Unit]]
+  private val semaphore = new AtomicInteger(workers)
 
   def shutdownNow(): Unit = {
-    execContext.shutdownNow()
+    val _ = execService.shutdownNow()
   }
 
-  def runTasks(tasks: List[Task]): List[Future[(Long, Try[Unit])]] = {
-    if (tasks.isEmpty) Nil
-    else tasks.map(t => Future(timed(t.run()))(execContext))
-  }
-
-  def timed(fn: => Unit): (Long, Try[Unit]) = {
-    val t1 = System.currentTimeMillis()
-    val res = Try(fn)
-    val t2 = System.currentTimeMillis()
-    (t2 - t1, res)
+  def run(task: Task): Future[Unit] = {
+    val promise = Promise[Unit]
+    val n = semaphore.get()
+    if (n > 0) {
+      if (semaphore.compareAndSet(n, n - 1)) {
+        val f = task.run(execService)
+        f.onComplete { _ =>
+          semaphore.incrementAndGet()
+          queue.dequeueFirst(_ => true).fold(())(_.success(()))
+        }
+        f
+      } else run(task)
+    } else {
+      val promise = Promise[Unit]
+      queue.enqueue(promise)
+      promise.future.flatMap(_ => run(task))
+    }
   }
 }
